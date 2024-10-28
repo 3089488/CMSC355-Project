b57ba09f0b61e6c3d01f501b3edd1075a50b852f
nick-biles <bilesn@vcu.edu>
2024-10-28 18:57:01 -0400

    Implemented printMedication method.

--- Sprint-1/myMedications.java
+++ Sprint-1/myMedications.java
@@ -1,16 +1,27 @@
 import java.util.ArrayList;
+import java.util.Date;
 
 public class myMedications {
+    static ArrayList<Medication> currentMedications;
     
     public static void main(String[] args) {
-        ArrayList<Medication> currentMedications = new ArrayList<Medication>();
+        currentMedications = new ArrayList<Medication>();
 
-        currentMedications.set(0, new Medication("testDrug", 15, Unit.mg));
+        currentMedications.add(new Medication("failure", -1, Unit.mg));
+        currentMedications.add(new Medication("testDrug", 15, Unit.mg));
+        currentMedications.add(new Medication("liquid drug test", new Date(), null, 100, Unit.ml, Frequency.Daily, null));
+        currentMedications.add(new Medication("Melatonin", new Date(), new Date(), 5, Unit.mg, Frequency.Evenings, "Only take as needed."));
+        currentMedications.add(new Medication("failure 2", -2, Unit.mg));
+
+        System.out.println();
+        printMedication(1, 3, currentMedications);
     }
 
-    int printMedication(int startIndex, int length) {
-
-
+    static int printMedication(int startIndex, int length, ArrayList<Medication> medications) {
+        for (int i = startIndex; i < startIndex + length; i++) {
+            medications.get(i).print();
+            System.out.println();
+        }
         return startIndex + length;
     }
 }
