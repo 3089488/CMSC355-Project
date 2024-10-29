046578a6ddf00e0ae1ea7d3b0d75ff3f6765f5fb
Steffen Niemeyer <steffenniemeyer@pm.me>
2024-10-29 01:03:12 -0400

    Created User, which will create a UUID based off of name. Within the new folder a new JSON is created. Added Maven to project.

--- Sprint-2/src/main/Medication.java
+++ Sprint-2/src/main/Medication.java
@@ -1,5 +1,6 @@
+package main;
+
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 public class Medication {
---
+++ Sprint-2/src/main/User.java
@@ -0,0 +1,39 @@
+package main;//import org.json.simple.JSONObject;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.UUID;
+import org.json.simple.JSONObject;
+
+public class User{
+
+    UUID uuid;
+    File userDirectory;
+    User(String name){
+        this.uuid = UUID.nameUUIDFromBytes(name.getBytes());
+
+        this.userDirectory = new File(this.uuid.toString());
+
+        if(!this.userDirectory.exists()){
+            this.userDirectory.mkdir();
+            System.out.println("Dir created.");
+
+            JSONObject currentMeds = new JSONObject();
+            currentMeds.put("Name", name);
+
+            try {
+                FileWriter currentMedsFile = new FileWriter(userDirectory.getPath() + "/Current.json");
+                currentMedsFile.write(currentMeds.toJSONString());
+                currentMedsFile.close();
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+        else System.out.println("Dir already exists.");
+    }
+
+    public static void main(String[] args)
+    {
+        User testUser = new User("Test User");
+    }
+}
