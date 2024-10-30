8697b29dfcb4878faa059504afd83297c74ddb09
nick-biles <bilesn@vcu.edu>
2024-10-30 14:32:53 -0400

    Updated myMedications

--- Sprint-1/myMedications.java
+++ Sprint-1/myMedications.java
@@ -5,28 +5,33 @@ import java.util.Scanner;
 public class myMedications {
     static ArrayList<Medication> currentMedications;
     static ArrayList<Medication> archivedMedications;
+    static Scanner userInput;
     
     public static void main(String[] args) {
         currentMedications = new ArrayList<Medication>();
         archivedMedications = new ArrayList<Medication>();
+        userInput = new Scanner(System.in);
 
         //Temp Medications until file system and managing support in Sprint 2.
-        currentMedications.add(new Medication("failure", -1, Unit.mg));
         currentMedications.add(new Medication("testDrug", 15, Unit.mg));
         currentMedications.add(new Medication("liquid drug test", new Date(), null, 100, Unit.ml, Frequency.Daily, null));
         currentMedications.add(new Medication("Melatonin", new Date(), new Date(), 5, Unit.mg, Frequency.Evenings, "Only take as needed."));
-        currentMedications.add(new Medication("failure 2", -2, Unit.mg));
+        currentMedications.add(new Medication("drug 1", -2, Unit.mg));
+        currentMedications.add(new Medication("drug 2", 2, Unit.mg));
+        currentMedications.add(new Medication("drug 3", 3, Unit.mg));
+        currentMedications.add(new Medication("drug 4", 4, Unit.mg));
 
         viewCurrentMedications();
+
+        userInput.close();
     }
 
     private static void viewCurrentMedications() {
         int currentMedicationIndex = 0;
-        Scanner userInput = new Scanner(System.in);
         String userChoice;
         boolean looping = true;
         do {
-            System.out.println("1. Edit");
+            System.out.println("1. Edit\n");
             printMedication(currentMedicationIndex, 3, currentMedications);
             System.out.println("2. Next Page");
             System.out.println("3. History");
@@ -55,7 +60,6 @@ public class myMedications {
 
     private static void viewArchivedMedications() {
         int currentMedicationIndex = 0;
-        Scanner userInput = new Scanner(System.in);
         String userChoice;
         boolean looping = true;
         do {
