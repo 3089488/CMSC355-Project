3a7bb75baa4d862c935665b119024fce823efa92
nick-biles <bilesn@vcu.edu>
2024-11-01 20:09:41 -0400

    Added main method to test cases file.

--- Sprint-1/testCases.java
+++ Sprint-1/testCases.java
@@ -2,6 +2,13 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class testCases {
+    //Main method to make running test cases easy.
+    public static void main(String[] args) {
+        testCases test = new testCases();
+
+        //Run only a single test case at a time.
+        test.testCase4();
+    }
     // Test case method for automated execution of the third test case.
     public void testCase3() {
         ArrayList<Medication> currentMedications = new ArrayList<Medication>();
@@ -15,8 +22,6 @@ public class testCases {
         // Check if error is caught when negative index is input.
         System.out.println("Output:");
         myMedications.printMedication(-1, 3, currentMedications);
-        System.out.println("Expected Output:");
-        System.out.println("Unset");
     }
 
     // Test case method for automated execution of the fourth test case.
@@ -27,15 +32,13 @@ public class testCases {
         currentMedications.add(new Medication("testDrug", 15, Unit.mg));
         currentMedications.add(new Medication("liquid drug test", new Date(), null, 100, Unit.ml, Frequency.Daily, null));
         currentMedications.add(new Medication("Melatonin", new Date(), new Date(), 5, Unit.mg, Frequency.Evenings, "Only take as needed."));
-        currentMedications.add(new Medication("drug 1", -2, Unit.mg));
-        currentMedications.add(new Medication("drug 2", 2, Unit.mg));
-        currentMedications.add(new Medication("drug 3", 3, Unit.mg));
-        currentMedications.add(new Medication("drug 4", 4, Unit.mg));
+        currentMedications.add(new Medication("drug 4", -2, Unit.mg));
+        currentMedications.add(new Medication("drug 5", 2, Unit.mg));
+        currentMedications.add(new Medication("drug 6", 3, Unit.mg));
+        currentMedications.add(new Medication("drug 7", 4, Unit.mg));
 
         // Check if 4, 5, and 6 medications in arrayList are printed.
         System.out.println("Output:");
         myMedications.printMedication(3, 3, currentMedications);
-        System.out.println("Expected Output:");
-        System.out.println("Unset");
     }
 }
